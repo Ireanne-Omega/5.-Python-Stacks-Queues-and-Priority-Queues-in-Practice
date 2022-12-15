3fc9a4200c9268870e60d92573f52f871194799c
Ireanne-Omega <116430505+Ireanne-Omega@users.noreply.github.com>
2022-12-16 00:41:08 +0800

    Exercise 2

--- Exercise 1.py
+++ Exercise 1.py
@@ -13,17 +13,15 @@ class Queue:
     def dequeue(self):
         return self._elements.popleft()
 
-
 # Exercise 2
-from queues import Queue
 
-# fifo enqueue statement
+from queue import Queue
+
 fifo = Queue()
 fifo.enqueue("1st")
 fifo.enqueue("2nd")
 fifo.enqueue("3rd")
 
-# fifo dequeu statement
 fifo.dequeue()
 '1st'
 fifo.dequeue()
@@ -31,27 +29,9 @@ fifo.dequeue()
 fifo.dequeue()
 '3rd'
 
-# Exercise 3
-# queues.py
 
-from collections import deque
 
-class Queue:
-    def __init__(self, *elements):
-        self._elements = deque(elements)
 
-    def __len__(self):
-        return len(self._elements)
-
-    def __iter__(self):
-        while len(self) > 0:
-            yield self.dequeue()
-
-    def enqueue(self, element):
-        self._elements.append(element)
-
-    def dequeue(self):
-        return self._elements.popleft()
 
 
 
