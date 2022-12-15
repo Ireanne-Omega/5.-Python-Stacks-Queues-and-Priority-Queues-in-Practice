5e964677c71d445e8c353e37a15b99302113fee0
Ireanne-Omega <116430505+Ireanne-Omega@users.noreply.github.com>
2022-12-16 01:48:08 +0800

    Exercise 10

--- Exercise 1.py
+++ Exercise 1.py
@@ -124,6 +124,15 @@ fruits
 
 # Exercise 10
 
+person1 = ("John", "Brown", 42)
+person2 = ("John", "Doe", 42)
+person3 = ("John", "Doe", 24)
+
+person1 < person2
+#True
+
+person2 < person3
+#False
 
 
 
