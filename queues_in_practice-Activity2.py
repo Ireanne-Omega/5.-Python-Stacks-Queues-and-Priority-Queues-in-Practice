e34c3dab0f718b23d9e0c90e5158e52f3a37f53c
Ireanne-Omega <116430505+Ireanne-Omega@users.noreply.github.com>
2022-12-16 19:21:09 +0800

    maps

--- queues_in_practice-Activity2.py
+++ queues_in_practice-Activity2.py
@@ -2,4 +2,38 @@
 # Reference: https://realpython.com/queue-in-python/#learning-about-the-types-of-queues
 
 # Exercise 1
-#
+
+# this is importing and refactoring
+from typing import NamedTuple
+
+class City(NamedTuple):
+    name: str
+    country: str
+    year: int | None
+    latitude: float
+    longitude: float
+
+    @classmethod
+    def from_dict(cls, attrs):
+        return cls(
+            name=attrs["xlabel"],
+            country=attrs["country"],
+            year=int(attrs["year"]) or None,
+            latitude=float(attrs["latitude"]),
+            longitude=float(attrs["longitude"]),
+        )
+
+# Exercise 2
+
+import networkx as nx
+
+def load_graph(filename, node_factory):
+    graph = nx.nx_agraph.read_dot(filename)
+    nodes = {
+        name: node_factory(attributes)
+        for name, attributes in graph.nodes(data=True)
+    }
+    return nodes, nx.Graph(
+        (nodes[name1], nodes[name2], weights)
+        for name1, name2, weights in graph.edges(data=True)
+    )
