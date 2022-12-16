5bf129cd548b470c88e03b21d8a59cf2edeffb4d
Ireanne-Omega <116430505+Ireanne-Omega@users.noreply.github.com>
2022-12-16 11:21:33 +0800

    Exercise 12

--- Exercise 1.py
+++ Exercise 1.py
@@ -150,7 +150,27 @@ class PriorityQueue:
     def dequeue(self):
         return heappop(self._elements)
     
-    
+
+# Exercise 12
+
+from queues import PriorityQueue
+
+CRITICAL = 3
+IMPORTANT = 2
+NEUTRAL = 1
+
+messages = PriorityQueue()
+
+messages.enqueue_with_priority(IMPORTANT, "Windshield wipers turned on")
+messages.enqueue_with_priority(NEUTRAL, "Radio station tuned in")
+messages.enqueue_with_priority(CRITICAL, "Brake pedal depressed")
+messages.enqueue_with_priority(IMPORTANT, "Hazard lights turned on")
+
+messages.dequeue()
+
+#
+
+
 
 
 
