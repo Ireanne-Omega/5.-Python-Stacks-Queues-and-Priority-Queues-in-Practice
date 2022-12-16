d6ef0e7f5cf085b547bb1e40c349efc736eef589
Ireanne-Omega <116430505+Ireanne-Omega@users.noreply.github.com>
2022-12-16 11:36:40 +0800

    Exercise 13

--- Exercise 1.py
+++ Exercise 1.py
@@ -168,7 +168,20 @@ messages.enqueue_with_priority(IMPORTANT, "Hazard lights turned on")
 
 messages.dequeue()
 
-#
+# Exercise 13
+
+class PriorityQueue:
+    def __init__(self):
+        self._elements = []
+
+    def enqueue_with_priority(self, priority, value):
+        heappush(self._elements, (-priority, value))
+
+    def dequeue(self):
+        return heappop(self._elements)[1]
+
+
+
 
 
 
