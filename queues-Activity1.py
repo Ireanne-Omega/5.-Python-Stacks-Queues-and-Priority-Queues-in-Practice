8ab5657873b9445108ac74ec17cce61980390a20
Ireanne-Omega <116430505+Ireanne-Omega@users.noreply.github.com>
2022-12-17 22:58:25 +0800

    # added for queues- Activity 1

--- queues-Activity1.py
+++ queues-Activity1.py
@@ -1,23 +1,76 @@
-# Programmed by: Ireanne N. Omega
-# BSCOE 2-2
+# Programmed by: Ireanne N. Omega BSCOE 2-2
 # Reference: https://realpython.com/queue-in-python/#learning-about-the-types-of-queues
 
-#  Queue Data Type
+# Exercise 1
+# QUEUES
+
+# this is for importing and refactoring
 from collections import deque
 
+
+class IterableMixin:
+    class IterableMixin:
+        def __len__(self):
+            return len(self._elements)
+
+        def __iter__(self):
+            while len(self) > 0:
+                yield self.dequeue()
+
+    class Queue(IterableMixin):
+        def __init__(self, *elements):
+            self._elements = deque(elements)
+
+        def __len__(self):
+            return len(self._elements)
+
+        def __iter__(self):
+            while len(self) > 0:
+                yield self.dequeue()
+
+        def enqueue(self, element):
+            self._elements.append(element)
+
+        def dequeue(self):
+            return self._elements.popleft()
+
+
+# Exercise 2
+# FIFO QUEUES FIRST EXAMPLE
+
+# enqueue statement
 class Queue:
-    def __init__(self):
-        self._elements = deque()
-
-    def enqueue(self, element):
-        self._elements.append(element)
-
-    def dequeue(self):
-        return self._elements.popleft()
+    pass
 
 
-# Queue Data Type - len
-from collections import deque
+fifo = Queue()
+fifo.enqueue("1st")
+fifo.enqueue("2nd")
+fifo.enqueue("3rd")
+
+# dequeue statement
+fifo.dequeue()
+fifo.dequeue()
+fifo.dequeue()
+
+# Exercise 3
+# FIFO QUEUE SECOND EXAMPLE
+
+fifo: Queue
+from queues import Queue
+
+fifo = Queue("1st", "2nd", "3rd")
+
+len(fifo)
+
+for element in fifo:
+    print(element)
+
+len(fifo)
+
+
+# Exercise 4
+# Class Queue and Input Statement
 
 class Queue:
     def __init__(self, *elements):
@@ -37,15 +90,120 @@ class Queue:
         return self._elements.popleft()
 
 
-# Stack Data Type
+# Exercise 5
+# Stack Data Type Queues
+
 class Stack(Queue):
     def dequeue(self):
         return self._elements.pop()
 
+
+# Exercise 6
+# Lifo Queue First Example
+
+lifo = Stack("1st", "2nd", "3rd")
+
+for element in lifo:
+    print(element)
+
+# Exercise 7
+# Lifo Queue Second Example
+
+lifo = []
+
+lifo.append("1st")
+lifo.append("2nd")
+lifo.append("3rd")
+
+lifo.pop()
+lifo.pop()
+lifo.pop()
+
+# Exercise 8
+# Priority Queue & Heap & Heappush
+
+# this is for importing and refactoring
+from heapq import heappush
+
+# Heappush
+fruits = []
+
+heappush(fruits, "orange")
+heappush(fruits, "apple")
+heappush(fruits, "banana")
+
+fruits
+
+# this is for importing and refactoring
+from heapq import heappop
+
+# Heappop
+heappop(fruits)
+
+fruits
+
+# Exercise 10
+# Tuple Comparison
+
+person1 = ("John", "Brown", 42)
+person2 = ("John", "Doe", 42)
+person3 = ("John", "Doe", 24)
+
+person1 < person2
+
+person2 < person3
+
+# Exercise 11
 # Priority Queue Data Type
+
+# this is for importing and refactoring
+from collections import deque
+from heapq import heappop, heappush
+
+
+class PriorityQueue(IterableMixin):
+    def __init__(self):
+        self._elements = []
+
+    def enqueue_with_priority(self, priority, value):
+        heappush(self._elements, (priority, value))
+
+    def dequeue(self):
+        return heappop(self._elements)[1]
+
+
+# Exercise 12
+# Priority Queue Data Type First Example
+
+from queues import PriorityQueue
+
+CRITICAL = 3
+IMPORTANT = 2
+NEUTRAL = 1
+
+messages = PriorityQueue()
+
+messages.enqueue_with_priority(IMPORTANT, "Windshield wipers turned on")
+messages.enqueue_with_priority(NEUTRAL, "Radio station tuned in")
+messages.enqueue_with_priority(CRITICAL, "Brake pedal depressed")
+messages.enqueue_with_priority(IMPORTANT, "Hazard lights turned on")
+
+messages.dequeue()
+
+messages.dequeue()
+
+messages.dequeue()
+
+messages.dequeue()
+
+# Exercise 13
+# Corner Cases in the Priority Queue
+
+# this is for importing and refactoring
 from collections import deque
 from heapq import heappop, heappush
 from itertools import count
+
 
 class PriorityQueue:
     def __init__(self):
@@ -59,71 +217,40 @@ class PriorityQueue:
     def dequeue(self):
         return heappop(self._elements)[-1]
 
-# Refactoring the Code Using a Mixin Class
-class IterableMixin:
-    def __len__(self):
-        return len(self._elements)
 
-    def __iter__(self):
-        while len(self) > 0:
-            yield self.dequeue()
+# Exercise 14
+# Data Classes
 
-class Queue(IterableMixin):
-    def __init__(self, *elements):
-        self._elements = deque(elements)
-
-    def enqueue(self, element):
-        self._elements.append(element)
-
-    def dequeue(self):
-        return self._elements.popleft()
-
-class Stack(Queue):
-    def dequeue(self):
-        return self._elements.pop()
-
-class PriorityQueue(IterableMixin):
-    def __init__(self):
-        self._elements = []
-        self._counter = count()
-
-    def enqueue_with_priority(self, priority, value):
-        element = (-priority, next(self._counter), value)
-        heappush(self._elements, element)
-
-    def dequeue(self):
-        return heappop(self._elements)[-1]
-
-from collections import deque
+# this is for importing and refactoring
 from dataclasses import dataclass
-from heapq import heapify, heappop, heappush
-from itertools import count
-from typing import Any
 
-@dataclass(order=True)
-class Element:
-    priority: float
-    count: int
-    value: Any
 
-class MutableMinHeap(IterableMixin):
-    def __init__(self):
-        super().__init__()
-        self._elements_by_value = {}
-        self._elements = []
-        self._counter = count()
+@dataclass
+class Message:
+    event: str
 
-    def __setitem__(self, unique_value, priority):
-        if unique_value in self._elements_by_value:
-            self._elements_by_value[unique_value].priority = priority
-            heapify(self._elements)
-        else:
-            element = Element(priority, next(self._counter), unique_value)
-            self._elements_by_value[unique_value] = element
-            heappush(self._elements, element)
 
-    def __getitem__(self, unique_value):
-        return self._elements_by_value[unique_value].priority
+# Exercise 15
 
-    def dequeue(self):
-        return heappop(self._elements).value
+# this is for importing and refactoring
+from dataclasses import dataclass
+
+
+@dataclass
+class Message:
+    event: str
+
+
+wipers = Message("Windshield wipers turned on")
+hazard_lights = Message("Hazard lights turned on")
+
+# Exercise 16
+# Messages
+
+messages = PriorityQueue()
+
+messages.enqueue_with_priority(CRITICAL, wipers)
+messages.enqueue_with_priority(IMPORTANT, hazard_lights)
+
+messages.enqueue_with_priority(CRITICAL, Message("ABS engaged"))
+
